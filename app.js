a56c2562803ef24cc2e9066f8fddde0a12402c44
Walter Fisher <o.b.i.e@Mac.lan>
2025-04-30 18:36:16 -0500

    Fixed response error.

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const app = express();
 //define a port number for the server to listen for a connection. 
 const PORT = 3000;
 //initialize and retain an index route to automatically render a message when the server starts
-app.get("/", (request, reponse, next) => {
+app.get("/", (request, response, next) => {
     response.send("This is the home page");//render a str on the page
-    response.json("Hello World, This is a JSON");
+    // response.json("Hello World, This is a JSON");
 });
 //have the app listen at the PORT where a console.log says `Server is listening on ${PORT}. Connection established.`
 app.listen(PORT, () => {
